efaa2f0b95d4529577f235785bd518e707043c3c
fahma20 <fahmaxo@gmail.com>
2024-10-31 14:11:58 +0300

    assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return (
+    <nav>
+      {[
+        { id: 1, href: '#home', text: 'home' },
+        { id: 2, href: '#about', text: 'about' },
+        { id: 3, href: '#projects', text: 'projects' },
+      ].map(link => (
+        <a key={link.id} href={link.href}>
+          {link.text}
+        </a>
+      ))}
+    </nav>
+  );
+};
 
 export default NavBar;
